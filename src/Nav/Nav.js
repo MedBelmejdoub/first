85ccf28205da8f25fde1ef1eb17c54b88200174b
Mohammed Belmejdoub <116885619+BelmejdoubMed@users.noreply.github.com>
2022-12-12 00:30:11 +0100

    add new elements

---
+++ src/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NewTodo from "../Todo/Page/NewTodo/NewTodo";
+const Nav = () => {
+  return (
+    <div>
+      <li>
+        <ul>
+          <a href="/Home">Todo</a>
+        </ul>
+        <ul>
+          <a href="/">test</a>
+        </ul>
+        <ul>
+          <a href="/">e</a>
+        </ul>
+        <ul>
+          <a href="/">a</a>
+        </ul>
+      </li>
+    </div>
+  );
+};
+
+export default Nav;
